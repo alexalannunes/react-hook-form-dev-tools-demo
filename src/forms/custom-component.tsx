d258c71b30a0587c835a7028ad1c20e7ce1f0427
Alex Alan Nunes <aalannunesdelima@apple.com>
2024-01-25 01:21:04 -0300

    add one field

--- src/forms/custom-component.tsx
+++ src/forms/custom-component.tsx
@@ -1,13 +1,64 @@
-import { Button, Container, Input, Stack } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Image,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { City, SelectCity } from "../components/custom-fields/select-city";
 import { useControlDispatch } from "../context/control-provider";
+import flamengo from "../../public/flamengo.png";
+import corinthians from "../../public/corinthians.svg";
+import saopaulo from "../../public/saopaulo.svg";
 
 type User = {
   name: string;
   city: City;
+  team: string;
 };
+
+interface Props {
+  value: string;
+  onChange: (v: string) => void;
+}
+function BestSoccerTeam({ value, onChange }: Props) {
+  const flag = [flamengo, corinthians, saopaulo];
+  return (
+    <Stack>
+      <Text>Melhor time do Brasil</Text>
+      <HStack>
+        {["flamengo", "corinthians", "São Paulo"].map((team, index) => (
+          <Flex
+            key={team}
+            rounded={"md"}
+            h="20"
+            w={"20"}
+            p={4}
+            alignItems={"center"}
+            justifyContent={"center"}
+            border={"1px"}
+            borderColor={"gray.400"}
+            cursor={"pointer"}
+            bg={value === team ? "cyan.100" : "white"}
+            onClick={() => {
+              onChange(team);
+            }}
+            _hover={{
+              borderColor: "red.400",
+            }}
+          >
+            <Image src={flag[index]} alt={team} />
+          </Flex>
+        ))}
+      </HStack>
+    </Stack>
+  );
+}
 
 export function BasicCustomComponentForm() {
   const dispatch = useControlDispatch();
@@ -39,6 +90,21 @@ export function BasicCustomComponentForm() {
             render={({ field }) => {
               return (
                 <SelectCity
+                  value={field.value}
+                  onChange={(city) => {
+                    field.onChange(city);
+                  }}
+                />
+              );
+            }}
+          />
+
+          <Controller
+            name="team"
+            control={control}
+            render={({ field }) => {
+              return (
+                <BestSoccerTeam
                   value={field.value}
                   onChange={(city) => {
                     field.onChange(city);
